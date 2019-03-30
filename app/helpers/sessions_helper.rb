19f7e20a27d29694e46aa64e3558d51cf72ed8fa
Tomoya Hoshino <tomoya1010@outlook.com>
2019-03-30 23:48:36 +0000

    Finish index edit

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,6 +8,9 @@ module SessionsHelper
           @current_user ||= User.find_by(id: session[:user_id])
       end
   end
+  def current_user?(user)
+    user == current_user
+  end
   
   def logged_in?
       !current_user.nil?
