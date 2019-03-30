32a6bcb2c588546d1b613746d88f7911ffe601aa
Tomoya Hoshino <tomoya1010@outlook.com>
2019-03-30 05:07:53 +0000

    use-SSL & Puma-Webserver in Production

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
+  get '/signup', to: 'users#new'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
